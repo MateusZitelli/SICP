ba7addc423cd61d171e6750cc57fa7ddd31485b0
Mateus Zitelli <zitellimateus@gmail.com>
2015-08-29 11:51:20 -0300

    Add ex. 1.19

--- cap1/exercises.scm
+++ cap1/exercises.scm
@@ -187,3 +187,19 @@
 (define (new-* a b) (iter-* a b 0))
 (new-* 42 24)
 
+; 1.19
+(define (fib n) (fib-iter 1 0 0 1 n))
+
+(define (fib-iter a b p q count)
+  (cond ((= count 0) b)
+        ((even? count)
+         (fib-iter a
+                   b
+                   (* p p)
+                   (+ (* q q) (* 2 p q))
+                   (/ count 2))
+         (else (fib-iter (+ (* b q) (* a q) (* a p))
+                         (+ (* b q) (* a q))
+                         p
+                         q
+                         (- count 1))))))
