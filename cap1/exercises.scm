0bc0093abcb4ccce2f4d27a2e53ca79a988ec022
Mateus Zitelli <zitellimateus@gmail.com>
2015-09-19 19:40:41 -0300

    Ex. 1.20-22

--- cap1/exercises.scm
+++ cap1/exercises.scm
@@ -197,9 +197,134 @@
                    b
                    (* p p)
                    (+ (* q q) (* 2 p q))
-                   (/ count 2))
-         (else (fib-iter (+ (* b q) (* a q) (* a p))
-                         (+ (* b q) (* a q))
-                         p
-                         q
-                         (- count 1))))))
+                   (/ count 2)))
+        (else (fib-iter (+ (* b q) (* a q) (* a p))
+                        (+ (* b q) (* a q))
+                        p
+                        q
+                        (- count 1)))))
+
+(fib-iter 1 0 1 1 4)
+
+; 1.20
+(define (gcd a b)
+  (if (= b 0)
+    a
+    (gcd b (remainder a b))))
+
+; Running it as (gcd 206 40)
+(gcd 206 40)
+(gcd 40 (remainder 206 40))
+(gcd
+  (remainder 206 40)
+  (remainder 40 (remainder 206 40)))
+(gcd
+  (remainder 40 (remainder 206 40))
+  (remainder 
+    (remainder 206 40)
+    (remainder 40 (remainder 206 40))))
+(gcd 
+  (remainder 
+    (remainder 206 40)
+    (remainder 40 (remainder 206 40)))
+  (remainder
+    (remainder 40 (remainder 206 40))
+    (remainder 
+      (remainder 206 40)
+      (remainder 40 (remainder 206 40))))) #| In this case the b = 0 |# 
+
+; Just a is evaluated
+(gcd (remainder 
+       (remainder 206 40)
+       (remainder 40 (remainder 206 40))))
+
+; So just 4 remainder operations are evaluated in normal-mode operation or lazy evaluation.
+
+(define (expmod base exp m)
+  (cond ((= exp 0) 1)
+        ((even? exp)
+         (remainder (square (expmod base (/ exp 2) m))
+                    m))
+        (else
+          (remainder (* base (expmod base (- exp 1) m))
+                     m))))
+
+(define (fermat-test n)
+  (define (try-it a)
+    (= (expmod a n n) a))
+  (try-it (+ 1 (random (- n 1)))))
+
+(define (fast-prime? n times)
+  (cond ((= times 0) #t)
+        ((fermat-test n) (fast-prime? n (- times 1)))
+        (else #f)))
+
+(fast-prime? 73 10)
+
+; Ex. 1.21
+(define (smallest-divisor n)
+  (find-divisor n 2))
+
+(define (find-divisor n test-divisor)
+  (cond ((> (square test-divisor) n) n)
+        ((divides? test-divisor n) test-divisor)
+        (else (find-divisor n (+ test-divisor 1)))))
+
+(define (divides? a b)
+  (= (remainder b a) 0))
+
+(smallest-divisor 199) ;199 
+(smallest-divisor 1999) ;1999 
+(smallest-divisor 19999) ;7 
+
+;1.22
+(define (prime? n)
+  (= n (smallest-divisor n)))
+
+(define (timed-prime-test n)
+  (newline)
+  (display n)
+  (let ((start-time (current-milliseconds)))
+   (start-prime-test n start-time)))
+
+(define (start-prime-test n start-time)
+  (if (prime? n)
+    (report-prime (- (current-milliseconds) start-time))))
+
+(define (report-prime elapsed-time)
+  (display " *** ")
+  (display elapsed-time))
+
+(define (search-for-primes start end)
+  (cond 
+    ((> start end) 
+     (newline))
+    ((odd? start)
+     (timed-prime-test start)
+     (search-for-primes (+ start 2) end))
+    (else (search-for-primes (+ start 1) end))))
+
+(search-for-primes 1e11 (+ 1e11 70))
+#|
+100000000003 *** 180.0
+100000000019 *** 152.0
+100000000057 *** 149.0
+|#
+(search-for-primes 1e12 (+ 1e12 70))
+#|
+1000000000039 *** 485.0
+1000000000061 *** 483.0
+1000000000063 *** 476.0
+|#
+(search-for-primes 1e13 (+ 1e13 100))
+#|
+10000000000037.0 *** 1535.0
+10000000000051.0 *** 1522.0
+10000000000099.0 *** 1564.0
+|#
+(define mean1e11 (/ (+ 180 152 149) 3))
+(define mean1e12 (/ (+ 485 483 476) 3))
+(define mean1e13 (/ (+ 1535 1522 1564) 3))
+
+(/ (/ mean1e12 mean1e11) (sqrt 10)) ; 0.949340736233501
+(/ (/ mean1e13 mean1e12) (sqrt 10)) ; 1.01197265011344 
