a2686359c1c38fc5174168aed754c4221ded1c6b
Mateus Zitelli <zitellimateus@gmail.com>
2015-10-06 10:59:28 -0300

    Add ex. 1.33

--- cap1/1.3.scm
+++ cap1/1.3.scm
@@ -50,12 +50,12 @@
 (define (accumulate combiner null-value term a next b)
   (define (iter a result)
     (if (> a b)
-      null-value
+      result
       (iter (next a) (combiner (term a) result))))
-  (iter a start))
+  (iter a null-value))
 
 (define (product term a next b)
-  (reducer * 1 term a next b))
+  (accumulate * 1 term a next b))
 
 (define (identity x) x)
 
@@ -64,6 +64,8 @@
 
 (define (square x) (* x x))
 
+(define (inc x) (+ x 1))
+
 (define (wallis-pi n)
   (define (term x)
     (/
@@ -71,11 +73,62 @@
       (- (* 4 (square x)) 1)))
   (* 2 (product term 1 inc n)))
 
-(wallis-pi 10)
+(wallis-pi 10000)
 
-; For a recursive version just change the reducer
+;1.32 For a recursive version just change the reducer
 (define (accumulate combiner null-value term a next b)
   (if (> a b)
     null-value
     (combiner (term a)
-        (reducer op start term (next a) next b))))
+        (accumulate combiner null-value term (next a) next b))))
+
+;1.33
+(define (filtered-accumulate combiner null-value term a next b filter-proc)
+  (define (iter a result)
+    (if (filter-proc a)
+     (if (> a b)
+      result
+      (iter (next a) (combiner (term a) result)))
+     (iter (next a) result)))
+  (iter a null-value))
+
+; fast-prime from 1.20
+(define (expmod base exp m)
+  (cond ((= exp 0) 1)
+        ((even? exp)
+         (remainder (square (expmod base (/ exp 2) m))
+                    m))
+        (else
+          (remainder (* base (expmod base (- exp 1) m))
+                     m))))
+
+(define (fermat-test n)
+  (define (try-it a)
+    (= (expmod a n n) a))
+  (try-it (+ 1 (random (- n 1)))))
+
+(define (fast-prime? n times)
+  (cond ((= times 0) #t)
+        ((fermat-test n) (fast-prime? n (- times 1)))
+        (else #f)))
+
+(define (prime? n)
+  (fast-prime? n 10))
+; End primes
+
+(define (prime-squared-sum a b)
+  (filtered-accumulate + 0 square a inc b prime?))
+
+(prime-squared-sum 1 100)
+
+(define (gcd a b)
+  (if (= b 0)
+    a
+    (gcd b (remainder a b))))
+
+(define (relative-primes-sum n)
+  (define (relative-prime x)
+    (= (gcd x n) 1))
+  (filtered-accumulate + 0 identity 1 inc n relative-prime))
+
+(relative-primes-sum 5)
